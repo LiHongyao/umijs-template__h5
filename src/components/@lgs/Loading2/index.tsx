8aca5c0bdb04e7886cca45a879b777a7f7c5e2b2
Li-HONGYAO <lihy_online@163.com>
2020-11-04 09:38:57 +0800

    更新

--- src/components/@lgs/Loading2/index.tsx
+++ src/components/@lgs/Loading2/index.tsx
@@ -1,20 +1,24 @@
-import React, { FC, memo } from 'react'
-import './index.less'
+import React, { FC, memo } from 'react';
+import './index.less';
 
 interface IProps {
-  showTips?: boolean,
-  tips?: string,
-  top?: number
+  showTips?: boolean;
+  tips?: string;
+  top?: string;
 }
-const Loading2: FC<IProps> = ({ top = 100, tips = '数据加载中...', showTips = true }) => {
+const Loading2: FC<IProps> = ({
+  top = '100px',
+  tips = '数据加载中...',
+  showTips = true,
+}) => {
   return (
-    <div className="lg-loading2" style={{paddingTop: `${top}px`}}>
+    <div className="lg-loading2" style={{ paddingTop: top }}>
       <div className="lg-loading2__ct">
         <div className="lg-loading2__icon"></div>
       </div>
-     {showTips && <div className="lg-loading2__tips">{tips}</div>}
+      {showTips && <div className="lg-loading2__tips">{tips}</div>}
     </div>
-  )
-}
+  );
+};
 
 export default memo(Loading2);
