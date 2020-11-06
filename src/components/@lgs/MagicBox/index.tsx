97713b1916749160ca4b9bdf4d6be20867ed005b
Li-HONGYAO <lihy_online@163.com>
2020-11-06 21:03:46 +0800

    新增 --- MagicBox，魔幻盒子，可根据子节点的个数自用变化矩阵

---
+++ src/components/@lgs/MagicBox/index.tsx
@@ -0,0 +1,75 @@
+import React, { FC, memo, useEffect, useRef, useState } from 'react';
+import './index.less';
+
+interface IProps {
+  children: JSX.Element | JSX.Element[];
+  maxColumns?: number;
+  gap?: number;
+}
+
+const MagicBox: FC<IProps> = props => {
+  // props
+  const { maxColumns = 3, gap = 10 } = props;
+  // refs
+  const lgWrapper = useRef<HTMLDivElement | null>(null);
+  // state
+  const [width, setWidth] = useState(0);
+
+  useEffect(() => {
+    if (lgWrapper.current) {
+      let type = Object.prototype.toString
+        .call(props.children)
+        .slice(8, -1)
+        .toLowerCase();
+      if (type === 'array') {
+        let children = props.children as JSX.Element[];
+        let w = lgWrapper.current.getBoundingClientRect().width;
+        let columns =
+          children.length < maxColumns ? children.length : maxColumns;
+        setWidth(() => {
+          return (w - (columns - 1) * gap) / columns;
+        });
+      }
+    }
+  }, [lgWrapper]);
+
+  const renderItems = () => {
+    let type = Object.prototype.toString
+      .call(props.children)
+      .slice(8, -1)
+      .toLowerCase();
+    if (type === 'array') {
+      let children = props.children as JSX.Element[];
+      let columns = children.length < maxColumns ? children.length : maxColumns;
+      return children.map((element: JSX.Element, i: number) => {
+        console.log((i + 1) % columns === 0);
+        return (
+          <section
+            className="lg-magic-box__item"
+            key={`lg-magic-box__item__${i}`}
+            style={{
+              width: width + 'px',
+              marginRight: (i + 1) % columns === 0 ? `0px` : `${gap}px`,
+            }}
+          >
+            {element}
+          </section>
+        );
+      });
+    } else {
+      return (
+        <section className="lg-magic-box__item" style={{ flex: 1 }}>
+          {props.children}
+        </section>
+      );
+    }
+  };
+  // render
+  return (
+    <div className="lg-magic-box" ref={lgWrapper}>
+      {renderItems()}
+    </div>
+  );
+};
+
+export default memo(MagicBox);
