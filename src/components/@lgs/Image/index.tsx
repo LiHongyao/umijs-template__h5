26630c53922821858d1a6509403fd3e0504da21f
lihongyao <lihy_online@163.com>
2020-12-30 17:54:22 +0800

    内容更新

--- src/components/@lgs/Image/index.tsx
+++ src/components/@lgs/Image/index.tsx
@@ -1,34 +1,55 @@
+import classNames from 'lg-classnames';
 import React, { memo, useState, useEffect, CSSProperties } from 'react';
 import './index.less';
 
 interface IProps {
-  src: string;
-  alt: string;
-  defaultImage?: any;
-  customCls?: string;
-  style?: CSSProperties;
-  onTap?: () => void;
+  src: string /** 图片地址 */;
+  defaultImage?: any /** 默认显示 */;
+  round?: boolean /** 是否呈现原型 */;
+  alt?: string /** alt属性 */;
+  type?: 'bg' | 'img' /** 类型 */;
+  customCls?: string /** 自定义class */;
+  style?: CSSProperties /** 自定义样式 */;
+  onTap?: () => void /** 监听点击 */;
 }
 const _Image: React.FC<IProps> = (props) => {
-  const { src, customCls, style, defaultImage, alt } = props;
-  const [innerSrc, setInnerSrc] = useState(defaultImage);
+  const { type = 'img' } = props;
+
+  const [innerSrc, setInnerSrc] = useState(props.defaultImage);
 
   useEffect(() => {
     const image = new Image();
     image.onload = () => {
       if (image.width > 1) {
-        setInnerSrc(src);
+        setInnerSrc(props.src);
       }
     };
-    image.src = src;
-  }, [src]);
+    image.src = props.src;
+  }, [props.src]);
 
-  return (
+  return type === 'img' ? (
+    // img呈现
     <img
-      className={`lg-image ${customCls || ''}`}
-      style={style}
+      className={classNames(['lg-image', props.customCls])}
+      style={props.style}
       src={innerSrc}
-      alt={alt}
+      alt={props.alt}
+      onClick={() => {
+        if (props.onTap) props.onTap();
+      }}
+    />
+  ) : (
+    // bg呈现
+    <div
+      className={classNames([
+        'lg-image-bg',
+        props.customCls,
+        { round: props.round },
+      ])}
+      style={{
+        background: `url(${innerSrc}) no-repeat center center / cover`,
+        ...props.style,
+      }}
       onClick={() => {
         if (props.onTap) props.onTap();
       }}
